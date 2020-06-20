c569ba9e7e4fc15c4191f2dd463a5bf3147c460e
FerhatGec <ferhatgectao@gmail.com>
2020-06-20 09:58:25 +0300

    Added make.scr and make.sh

---
+++ make.sh
@@ -0,0 +1,8 @@
+#   MIT License
+#   Customizable ScriftLang Build File
+#   Copyright (c) 2020 Ferhat Geçdoğan All Rights Reserved.
+#   Distributed under the terms of the MIT License.
+# 
+#!/bin/sh
+
+g++ example.cpp -o ex && ./ex
